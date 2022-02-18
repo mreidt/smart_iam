8b984f33e5beeb260ac67a2d4a1d63576df32446
Matheus Eidt <matheus@mx2.unisc.br>
2022-02-18 17:18:50 -0300

    test: testing user models.

---
+++ smart_iam/settings/testing.py
@@ -0,0 +1,16 @@
+from smart_iam.settings.development import * # noqa
+
+TEST_RUNNER = "xmlrunner.extra.djangotestrunner.XMLTestRunner"
+TEST_OUTPUT_DIR = "./reports/"
+TEST_OUTPUT_FILENAME = "unit.xml"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "test_db.sqlite3", # noqa
+    }
+}
+
+INSTALLED_APPS += ["behave_django"] # noqa
+
+DEBUG = False
