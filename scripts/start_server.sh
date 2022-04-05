36ea8ae6528d67997d2803dafa8dccc6208eccfc
Matheus Eidt <matheus@mx2.unisc.br>
2022-04-05 20:44:43 -0300

    chore: Including sample db data.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,7 +7,7 @@ python manage.py migrate --settings="smart_iam.settings.$MODE"
 
 if [ "$MODE" == "development" ];
 then
-    uwsgi --ini uwsgi.ini --py-autoreload=2 --honour-stdin --workers 1 --threads 2
+    uwsgi --ini uwsgi.ini --py-autoreload=2 --honour-stdin --reload-mercy 3 --workers 2 --threads 1
 else
     uwsgi --ini uwsgi.ini --workers 3 --threads 2
 fi
