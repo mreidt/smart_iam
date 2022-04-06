c2f222c88cd8cec2b3f51a6523737b747ab1c5c8
Matheus Reis Eidt <matheus@mx2.unisc.br>
2022-04-05 21:18:47 -0300

    version 0.0.3 (#4)
    * feat: creating products endpoints.
    
    * feat: including user permissions mixin.
    
    * test: creating tests for products.
    
    * style: runningn linters.
    
    * chore: changing plural name of the models.
    
    * feat: changing description to optional field and add delete endpoint.
    
    * test: creating test for deletion.
    
    * style: running linters.
    
    * feat: creating permissions endpoints.
    
    * fix: error in lookup field.
    
    * test: fix error in test and creating list tests.
    
    * test: creating tests for permissions.
    
    * test: fix test models string representation.
    
    * test: creating tests to account repositories.
    
    * test: creating tests for permissions handlers.
    
    * style: running linters.
    
    * chore: change import to import only the necessary exception.
    
    * test: tesnting permissions handlers.
    
    * chore: change import to import only the necessary exception.
    
    * test: testing products handlers.
    
    * chore: changing method name to improve readabillity and deleting unnecessary methods.
    
    * test: creating user repositories tests.
    
    * feat: creating Account and AccountProducts endpoints.
    
    * test: creating tests for account.
    
    * chore: changing business rules for account_product.
    
    * test: testing account_products endpoints.
    
    * chore: Create string representation to Account Products.
    
    * fix: Fixing wrong permissions validations.
    
    * feat: Including get product by name repository.
    
    * test: Creating tests for products repositories.
    
    * chore: Including sample db data.
    
    * chore: Configuring log.
    
    * feat: Creating user endpoints.
    
    * feat: including user permissions mixin.
    
    * style: runningn linters.
    
    * feat: changing description to optional field and add delete endpoint.
    
    * style: running linters.
    
    * feat: creating permissions endpoints.
    
    * style: running linters.
    
    * style: running linters.
    
    * test: Testing user repositories.

--- tests/unit/user/test_repositories.py
+++ tests/unit/user/test_repositories.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 
+from apps.account.models import IAMAccount
 from apps.permissions.models import Permissions
 from apps.products.models import Products
-from apps.user.models import CustomUser, UserPermissions
-from apps.user.repositories import custom_user_repository, user_permission_repository
+from apps.user.models import CustomUser, UserAccount, UserPermissions
+from apps.user.repositories import (
+    custom_user_repository,
+    user_account_repository,
+    user_permission_repository,
+)
 
 
 class TestCustomUserRepository(TestCase):
@@ -12,9 +17,22 @@ class TestCustomUserRepository(TestCase):
         cls.user = CustomUser.objects.create(
             email="teste@email.com", password="test1234"
         )
+        cls.account = IAMAccount.objects.create(email="teste@email.com")
+        cls.user_account = UserAccount.objects.create(
+            user=cls.user, account=cls.account
+        )
 
     def test_get_user_by_id(self):
         self.assertEqual(self.user, custom_user_repository.get_user_by_id(self.user.id))
+
+    def test_get_users_from_account(self):
+        user_list = custom_user_repository.get_users_from_account(self.account.id)
+        self.assertEqual(1, len(user_list))
+        self.assertEqual(self.user, user_list[0])
+
+    def test_delete_user(self):
+        custom_user_repository.delete(self.user)
+        self.assertEqual(0, CustomUser.objects.count())
 
 
 class TestUserPermissionRepository(TestCase):
@@ -37,3 +55,45 @@ class TestUserPermissionRepository(TestCase):
         )
         self.assertEqual(len(user_permissions), 1)
         self.assertEqual(self.user_permission, user_permissions[0])
+
+
+class TestUserAccountsRepository(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create(
+            email="teste@email.com", password="test1234"
+        )
+        cls.account = IAMAccount.objects.create(email="teste@email.com")
+        cls.user_account = UserAccount.objects.create(
+            user=cls.user, account=cls.account
+        )
+
+    def test_get_user_account_by_user_id(self):
+        self.assertEqual(
+            self.user_account,
+            user_account_repository.get_user_account_by_user_id(self.user.id),
+        )
+
+    def test_get_user_accounts_by_user_id(self):
+        user_accounts = user_account_repository.get_user_accounts_by_user_id(
+            self.user.id
+        )
+        self.assertEqual(1, len(user_accounts))
+        self.assertEqual(self.user_account, user_accounts[0])
+
+    def test_get_user_account_by_id(self):
+        self.assertEqual(
+            self.user_account,
+            user_account_repository.get_user_account_by_id(self.user_account.id),
+        )
+
+    def test_create_user_account(self):
+        new_user_account = user_account_repository.create_user_account(
+            self.user, self.account
+        )
+        self.assertEqual(2, UserAccount.objects.count())
+        self.assertIn(new_user_account, UserAccount.objects.all())
+
+    def test_delete_user_account(self):
+        user_account_repository.delete(self.user_account)
+        self.assertEqual(0, UserAccount.objects.count())
